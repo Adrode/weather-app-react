68f00d32152490213d4587f2aa59ca10194aae9b
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-03-10 18:38:35 +0100

    theme and ThemeProvider

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+    color: {
+        backgroung: "#0080FF",
+        border: "#404040",
+        fontColor: "#202020",
+    },
+    fontSize: {
+        normal: 400,
+        bold: 700,
+    },
+    fontSize: {
+        small: 18,
+        normal: 22,
+        big: 26,
+    },
+    breakpoints: {
+        //to be continued
+    }
+}
