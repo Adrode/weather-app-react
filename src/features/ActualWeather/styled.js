74b3bedf21afd867db1ec122de6ee1c0e9de2a36
Adrian Woźniak <adrianwozniak20@gmail.com>
2025-03-12 22:40:14 +0100

    StyledActualWeather styling

---
+++ src/features/ActualWeather/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledActualWeather = styled.section`
+    margin: 0 auto 40px;
+    border: 1px solid red;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
